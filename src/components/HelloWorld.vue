69c7c471cf8dfee01dc3381deb59674e8cd799cf
dindanausa <dinda.nausa.888@gmail.com>
2024-04-24 14:17:15 +0700

    Commit pertama

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,14 +27,20 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+
+    <LoginComponent />
   </div>
 </template>
 
 <script>
+import LoginComponent from './LoginComponent.vue';
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    LoginComponent
   }
 }
 </script>
